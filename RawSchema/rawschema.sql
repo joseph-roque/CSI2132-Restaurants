c1c863453f19eee9ebd979b81816c1cdc75959e7
James Reinlein <jamesreinlein@gmail.com>
2015-03-19 13:29:14 -0400

    Second Revision to database schema
    Still need to modify the REGEX for valid restaurant names
    :v:

--- RawSchema/rawschema.sql
+++ RawSchema/rawschema.sql
@@ -37,7 +37,8 @@ CREATE TABLE Restaurant
 	FOREIGN KEY (cuisine) REFERENCES CuisineType(cuisine_id)
 		ON UPDATE CASCADE ON DELETE RESTRICT,
 	CONSTRAINT valid_name CHECK (name ~* '^[A-Za-z][A-Za-z0-9 ]*$')
-		--Stats with letter, then alphanumeric with spaces
+		--Starts with letter, then alphanumeric with spaces
+		-- TODO: Add ' to viable names, and french accents?
 );
 
 CREATE TABLE CuisineType
@@ -50,36 +51,35 @@ CREATE TABLE CuisineType
 
 CREATE TABLE Rating
 (
-	user_id INTEGER NOT NULL,
+	user_id INTEGER NOT NULL DEFAULT '00',
 	post_date TIMESTAMP NOT NULL,
 	price SMALLINT NOT NULL DEFAULT 0,
 	food SMALLINT NOT NULL DEFAULT 0,
 	mood SMALLINT NOT NULL DEFAULT 0,
 	staff SMALLINT NOT NULL DEFAULT 0,
-	comments TEXT NOT NULL, -- Must enter comments! Should we enforce a min. length?
+	comments TEXT NOT NULL,
 	restaurant_id INTEGER NOT NULL,
 	PRIMARY KEY (user_id, post_date),
 	FOREIGN KEY (user_id) REFERENCES Rater(user_id)
-		ON UPDATE CASCADE ON DELETE RESTRICT, --What do we want?
+		ON UPDATE CASCADE ON DELETE SET DEFAULT,
 	FOREIGN KEY (restaurant_id) REFERENCES Restaurant(restaurant_id)
-		ON UPDATE CASCADE ON DELETE CASCADE -- no restaurant, no ratings
-	CONSTRAINT price_valid_rating CHECK (price >= 1 AND price <= 5)
-	CONSTRAINT food_valid_rating CHECK (food >= 1 AND food <= 5)
-	CONSTRAINT mood_valid_rating CHECK (mood >= 1 AND mood <= 5)
-	CONSTRAINT staff_valid_rating CHECK (staff >= 1 AND staff <= 5)
-
-	
+		ON UPDATE CASCADE ON DELETE CASCADE, -- no restaurant, no ratings
+	CONSTRAINT price_valid_rating CHECK (price >= 1 AND price <= 5),
+	CONSTRAINT food_valid_rating CHECK (food >= 1 AND food <= 5),
+	CONSTRAINT mood_valid_rating CHECK (mood >= 1 AND mood <= 5),
+	CONSTRAINT staff_valid_rating CHECK (staff >= 1 AND staff <= 5),
+	CONSTRAINT comments_min_length CHECK (DATALENGTH(comments) > 50)
 );
 
 CREATE TABLE Location
 (
 	location_id SERIAL,
 	first_open_date TIMESTAMP NOT NULL,
-	manager_name VARCHAR(70), --not sure about this
-	phone_number CHAR(11), --not sure about this
+	manager_name VARCHAR(70), 
+	phone_number CHAR(11), 
 	street_address TEXT, --not sure about this
-	hour_open TEXT, --not sure about this
-	hour_close TEXT, --not sure about this
+	hour_open DECIMAL(4,0), -- 24h format (eg 0630 is 6:30 am)
+	hour_close DECIMAL(4,0), -- 24h format (eg 1801 is 6:01 pm)
 	restaurant_id INTEGER NOT NULL,
 	PRIMARY KEY (location_id),
 	FOREIGN KEY (restaurant_id) REFERENCES Restaurant(restaurant_id)
@@ -95,16 +95,21 @@ CREATE TABLE MenuItem
 (
 	item_id SERIAL,
 	name VARCHAR(70) NOT NULL,
-	sort VARCHAR(8),
-	category TEXT, -- make refer to another table?
+	type_id VARCHAR(8), -- entree, main meal, beverage, dessert, etc.
 	description TEXT, --not sure about this
 	price DECIMAL(4,2),
 	restaurant_id INTEGER NOT NULL,
 	PRIMARY KEY (item_id),
 	FOREIGN KEY (restaurant_id) REFERENCES Restaurant(restaurant_id)
 		ON UPDATE CASCADE ON DELETE CASCADE, 
-		-- if restaurant no longer exists, neither does the menu item!
-	CONSTRAINT food_or_bev CHECK (sort='Food' OR sort='Beverage')
+	FOREIGN KEY (type_id) REFERENCES ItemType(type_id)
+);
+
+CREATE TABLE ItemType
+(
+	type_id SERIAL,
+	description TEXT NOT NULL,
+	PRIMARY KEY (type_id)
 );
 
 CREATE TABLE RatingItem
@@ -113,11 +118,11 @@ CREATE TABLE RatingItem
 	post_date TIMESTAMP NOT NULL,
 	item_id INTEGER NOT NULL,
 	rating SMALLINT NOT NULL,
-	comments TEXT, -- allow to be null (?)
+	comments TEXT,
 	PRIMARY KEY (user_id, post_date, item_id),
 	FOREIGN KEY (user_id) REFERENCES Rater(user_id)
-		ON UPDATE CASCADE ON DELETE RESTRICT, --What do we want?
+		ON UPDATE CASCADE ON DELETE CASCADE, 
 	FOREIGN KEY (item_id) REFERENCES MenuItem(item_id)
-		ON UPDATE CASCADE ON DELETE RESTRICT, --What do we want?
+		ON UPDATE CASCADE ON DELETE CASCADE, 
 	CONSTRAINT valid_rating CHECK (rating >= 1 AND rating <= 5)
 );
