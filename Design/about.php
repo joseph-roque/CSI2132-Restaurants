4e255b22c9358c4b5b1acdd533c335c9f6744181
James Reinlein <jamesreinlein@gmail.com>
2015-04-01 21:28:59 -0400

    ABOUT page started
    :v:

---
+++ Design/about.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Sizzl | About</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="">
+	<meta name="author" content="">
+
+	<!--link rel="stylesheet/less" href="less/bootstrap.less" type="text/css" /-->
+	<!--link rel="stylesheet/less" href="less/responsive.less" type="text/css" /-->
+	<!--append ‘#!watch’ to the browser URL, then refresh the page. -->
+	
+	<!-- SWITCH TO MIN EVENTUALLY? -->
+	<link href="css/bootstrap.css" rel="stylesheet">
+	<link href="css/style.css" rel="stylesheet">
+
+	<!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+	<!--[if lt IE 9]>
+	<script src="js/html5shiv.js"></script>
+	<![endif]-->
+
+	<!-- Fav and touch icons -->
+	<link rel="apple-touch-icon-precomposed" sizes="144x144" href="img/apple-touch-icon-144-precomposed.png">
+	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="img/apple-touch-icon-114-precomposed.png">
+	<link rel="apple-touch-icon-precomposed" sizes="72x72" href="img/apple-touch-icon-72-precomposed.png">
+	<link rel="apple-touch-icon-precomposed" href="img/apple-touch-icon-57-precomposed.png">
+	<link rel="shortcut icon" href="img/favicon.png">
+	
+	<!--<script src="js/jquery-2.1.3.js"></script>-->
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+
+	<script type="text/javascript" src="js/jquery.min.js"></script>
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="js/scripts.js"></script>
+</head>
+
+<body>
+<div class="container">
+	<div class="row clearfix">
+		<!-- Logo & subtitle text -->
+		<div class="col-md-12 column">
+				<img class="img-responsive center-block" src="logo.png" style="width:20%; padding-bottom:5px" />
+		</div>
+		
+		<div class="col-md-12 column">
+			<!-- Navigation Bar -->
+			<nav class="navbar navbar-default" role="navigation">
+				<div class="navbar-collapse collapse">
+					<!-- Search Bar -->
+					<ul class="navbar-brand">
+						<form class="navbar-form" role="search" method="get" id="search-form" name="search-form">
+							<div class="input-group">
+								<input type="text" class="form-control" placeholder="Restaurants, Cuisines..."
+								id="query" name="query" value="">
+									<div class="input-group-btn">
+								<button type="submit" class="btn btn-warning"><span class="glyphicon glyphicon-search"></span></button>
+								</div>
+							</div>
+						</form>
+					</ul>
+					<!-- Left -->
+					<ul class="nav navbar-nav navbar-left">
+						<li><a href="index.php">Home</a></li>
+						<li><a href="about.php"><b>About</b></a></li>
+						<li><a href="contact.php">Contact</a></li>
+					</ul>
+					<!-- Right -->
+					<ul class="nav navbar-nav navbar-right">
+						<li><a href="login.php">Login</a></li>
+						<li><a href="register.php">Register</a></li>
+					</ul>
+				</div>	
+			</nav>
+			
+			<h2 class="text-center text-info">
+					About the team
+			</h2>
+			
+			<div class="the-team">
+				
+				<!-- James -->
+				<div class="col-md-4">
+					<div class="thumbnail">
+					<a href="https://github.com/jreinlein/">
+					<div class="cropped-img" style="background-image:url('https://avatars0.githubusercontent.com/u/7434338?v=3&s=460')" /> </div>
+
+					<div class="caption">
+						<h2>
+							James Reinlein</a>
+						</h2>
+						<p>
+							Specialized in dank ass hamburgers, Restaurant 1 is world renown for its affordable finger foods.
+						</p>
+					</div>
+				</div>
+			</div>
+				<!-- Joseph -->
+				<div class="col-md-4">
+					<div class="thumbnail">
+					<a href="https://github.com/joseph-roque/">
+					<div class="cropped-img" style="background-image:url('http://josephroque.ca/assets/jroque_headshot_380x380.png')" /> </div>
+
+					<div class="caption">
+						<h2>
+							Joseph Roque</a>
+						</h2>
+						<p>
+							Better known locally as "Pho Ling Do Bang Do Ding Da Do 2", this hole-in-the-wall eatery features some of the grimiest pho known to Ottawa.
+						</p>
+					</div>
+				</div>
+				</div>
+				<!-- Moe -->
+				<div class="col-md-4">
+					<div class="thumbnail">
+					<a href="#">
+					<div class="cropped-img" style="background-image:url('moe.png')" /> </div>
+						
+					<div class="caption">
+						<h2>
+							"Moe" Shanti</a>
+						</h2>
+						<p>
+							You haven't been to a sports bar until you've been to Restaurant 3. A new addition to the downtown nightlife, this sports bar is praised for
+							its super hot waitresses with unreasonably-sized jugs. No comment on the food.
+						</p>
+
+					</div>
+				</div>
+			
+			<!-- Bottom Logo -->
+			<nav class="navbar-fixed-bottom">
+					<img style="max-width:50px"
+						 src="logo-fire.png">
+			</nav>
+	</div>
+</div>
+
+	
+
+</body>
+</html>
