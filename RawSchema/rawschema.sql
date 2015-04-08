705275bbeb5577dc46957f0cded5dcb89275a2ef
Joseph Roque <github@josephroque.ca>
2015-04-08 13:23:34 -0400

    Changed order of table creates
    Rating table was being created before Location table, so the foreign key constraint in Rating was failing

--- RawSchema/rawschema.sql
+++ RawSchema/rawschema.sql
@@ -49,6 +49,25 @@ CREATE TABLE Restaurant
 		-- TODO: Add ' to viable names, and french accents?
 );
 
+CREATE TABLE Location
+(
+	location_id SERIAL,
+	first_open_date TIMESTAMP NOT NULL,
+	manager_name VARCHAR(70), 
+	phone_number VARCHAR(16),
+	street_address TEXT, --not sure about this
+	hour_open DECIMAL(4,0), -- 24h format (eg 0630 is 6:30 am)
+	hour_close DECIMAL(4,0), -- 24h format (eg 1801 is 6:01 pm)
+	restaurant_id INTEGER NOT NULL,
+	PRIMARY KEY (location_id),
+	FOREIGN KEY (restaurant_id) REFERENCES Restaurant(restaurant_id)
+		ON UPDATE CASCADE ON DELETE CASCADE, -- no restaurant = no location
+	CONSTRAINT valid_phone CHECK (phone_number ~* E'^1?\\d{10}(x\\d{1,4}|)$')
+		-- When user inputs phone number in text field, before we put it in the database
+		-- we get rid of any spaces, dashes, etc. and just check if it's 10 digits with/without 1 at the start and extension at end
+		-- Accepts following formats: 16135550123 / 6135550123 / 6135550123x00 / 16135550123x1234
+);
+
 CREATE TABLE Rating
 (
 	user_id INTEGER NOT NULL DEFAULT 0,
@@ -69,25 +88,6 @@ CREATE TABLE Rating
 	CONSTRAINT mood_valid_rating CHECK (mood >= 0 AND mood <= 5),
 	CONSTRAINT staff_valid_rating CHECK (staff >= 0 AND staff <= 5),
 	CONSTRAINT comments_min_length CHECK (comments ~* '.{50,}')
-);
-
-CREATE TABLE Location
-(
-	location_id SERIAL,
-	first_open_date TIMESTAMP NOT NULL,
-	manager_name VARCHAR(70), 
-	phone_number VARCHAR(16),
-	street_address TEXT, --not sure about this
-	hour_open DECIMAL(4,0), -- 24h format (eg 0630 is 6:30 am)
-	hour_close DECIMAL(4,0), -- 24h format (eg 1801 is 6:01 pm)
-	restaurant_id INTEGER NOT NULL,
-	PRIMARY KEY (location_id),
-	FOREIGN KEY (restaurant_id) REFERENCES Restaurant(restaurant_id)
-		ON UPDATE CASCADE ON DELETE CASCADE, -- no restaurant = no location
-	CONSTRAINT valid_phone CHECK (phone_number ~* E'^1?\\d{10}(x\\d{1,4}|)$')
-		-- When user inputs phone number in text field, before we put it in the database
-		-- we get rid of any spaces, dashes, etc. and just check if it's 10 digits with/without 1 at the start and extension at end
-		-- Accepts following formats: 16135550123 / 6135550123 / 6135550123x00 / 16135550123x1234
 );
 
 CREATE TABLE ItemType
