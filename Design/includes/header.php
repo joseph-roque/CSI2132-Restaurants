5b4ae08299d036ab956780a2b659dadf1513c999
James Reinlein <jamesreinlein@gmail.com>
2015-04-08 17:13:26 -0400

    Attempt to include() using PHP

---
+++ Design/includes/header.php
@@ -0,0 +1,28 @@
+<!-- Navigation Bar -->
+<nav class="navbar navbar-default" role="navigation">
+	<div class="navbar-collapse collapse">
+		<!-- Search Bar -->
+		<ul class="navbar-brand">
+			<form class="navbar-form" role="search" method="get" id="search-form" name="search-form">
+				<div class="input-group">
+					<input type="text" class="form-control" placeholder="Restaurants, Cuisines..."
+					id="query" name="query" value="">
+						<div class="input-group-btn">
+					<button type="submit" class="btn btn-warning"><span class="glyphicon glyphicon-search"></span></button>
+					</div>
+				</div>
+			</form>
+		</ul>
+		<!-- Left -->
+		<ul class="nav navbar-nav navbar-left">
+			<li><a href="index.php">Home</a></li>
+			<li><a href="about.php">About</a></li>
+			<li><a href="contact.php"><b>Contact</b></a></li>
+		</ul>
+		<!-- Right -->
+		<ul class="nav navbar-nav navbar-right">
+			<li><a href="login.php">Login</a></li>
+			<li><a href="register.php">Register</a></li>
+		</ul>
+	</div>	
+</nav>
---
+++ Design/includes/logo.php
@@ -0,0 +1,2 @@
+<!-- Logo & subtitle text -->
+	<img class="img-responsive center-block" src="logo.png" style="width:20%; padding-bottom:5px" />
