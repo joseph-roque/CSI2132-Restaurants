6ce26188ad98f4d92c55c7b30bfe319b649bd933
Mohammed Shanti <mshan072@uottawa.ca>
2015-04-09 17:28:06 -0400

    re

--- Design/includes/edit-menu.php
+++ Design/includes/edit-menu.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 echo '
 <button onclick="redirectMenu(';
@@ -7,8 +6,6 @@ echo '
 	<span class=" glyphicon glyphicon-edit"></span>
 </button>';
 ?>
-=======
 <button  onclick = "redirect('review-item.php')" name = "edit-item" method  = "post"  type="edit-item" class="btn" style="padding-bottom:5px;padding-top:5px">
 	<span class=" glyphicon glyphicon-pencil"></span>
 </button>
->>>>>>> origin/master
