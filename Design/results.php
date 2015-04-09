5395953d42717f3003164187f1e8c48560ba05b4
James Reinlein <jamesreinlein@gmail.com>
2015-04-09 14:05:39 -0400

    Results design finished
    Need to connect  to DB

--- Design/results.php
+++ Design/results.php
@@ -15,17 +15,28 @@ if(array_key_exists('name', $_SESSION) && array_key_exists('userid',$_SESSION)){
 <body>
 <div class="container">
 	<div class="row clearfix">
-			<?php include("includes/header.php");?>
-			<?php include("includes/navbar.php");?>
-			<div class="col-md-12 column">
-			
-			<h2 class="text-center text-info">
-				[count] restaurants for "[given query]"
-			</h2>
-			
-					hi
+		<?php include("includes/header.php");?>
+		<?php include("includes/navbar.php");?>
+		<div class="col-md-12 column">
+				<h2 class="text-center text-info" style="margin-bottom:20px">
+					[count] restaurants for "[given query]"
+				</h2>
+				NOTE: SHOW THE QUERY IN THE SEARCH BAR!!!
+				<!-- Results for all restaurants matching query -->
+				<div class="well well-sm" style="line-height:1.75; font-size:16px">
+					<strong>[Restaurant Name w/ link]</strong> ([avg. rating]/5 with [count] reviews) <br>
+					[cuisine w/ link] <br>
+					[address] <br>
+					[hours open] - [hours close]
+				</div>			
+				<!-- ADD RESTAURANT bottom right -->
+			<div class="pull-right">
+				<strong>Can't find the restaurant you're looking for?
+				<button  onclick = "redirect()" name = "add-item" method  = "post"  type="add-item" class="btn btn-primary" style="margin-left:10px">
+					<span class="glyphicon glyphicon-plus" style="margin-right:10px"></span>Add a Restaurant</strong>
+				</button>
 			</div>
-			
+		</div>
 	</div>
 </div>
 
