60c50190b5388dc18584731dbd7704bad8486d68
Joseph Roque <github@josephroque.ca>
2015-04-11 20:26:27 -0400

    Turned header into link to main page

--- Design/includes/header.php
+++ Design/includes/header.php
@@ -1,2 +1,2 @@
 <!-- Logo & subtitle text -->
-<img class="img-responsive center-block" src="media/logo.png" style="width:20%; padding-bottom:5px" />
+<a href="index.php"><img class="img-responsive center-block" src="media/logo.png" style="width:20%; padding-bottom:5px" /></a>
