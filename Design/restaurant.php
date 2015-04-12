123a8cbbcbb4c6fd4bff540b24a96e93cfd8b69e
James Reinlein <jamesreinlein@gmail.com>
2015-04-12 18:28:57 -0400

    Added button to remove menu items :x:

--- Design/restaurant.php
+++ Design/restaurant.php
@@ -236,7 +236,8 @@
 							echo "<th><a href='restaurant.php?id=$id&sort=type'>Type</a></th>";
 							echo "<th><a href='restaurant.php?id=$id&sort=rating'>Rating</a></th>";
 						?>
-						<th>View</th>
+						<th />
+						<th />
 					</tr>
 				</thead>
 				<!-- All menu items -->
@@ -294,6 +295,11 @@
 
 								include("includes/edit-menu.php");
 
+								echo "</td>
+								<td>";
+								
+								include("includes/delete-menu.php");
+								
 								echo "</td></tr>";
 					}
 
