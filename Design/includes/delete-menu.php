be6493c20d235ed4c19b05b58ab416ad63736fa7
Joseph Roque <github@josephroque.ca>
2015-04-13 10:11:26 -0400

    Deleted unused files

--- Design/includes/delete-menu.php
+++
@@ -1,8 +0,0 @@
-<?php
-echo '
-<button onclick="redirectMenu(';
-	echo "'review-item.php?id=$itemid'";
-	echo ')"  name = "edit-item" method  = "post"  type="edit-item" class="btn" style="padding-bottom:5px;padding-top:5px">
-	<span class=" glyphicon glyphicon-remove"></span>
-</button>';
-?>
